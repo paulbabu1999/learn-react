8cd7dc34363b7fb815d7cc40d51bbed926be703f
paul babu <paulbabu.1999@gmail.com>
2024-02-14 16:13:37 -0500

    add in class changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,15 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Counter from './components/snapshot'
+import Form from './components/snapshot2'
+import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Scoreboard />
     </div>
   )
 }
